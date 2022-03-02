d406507532d12733246f4fac6923be5a2994179e
padosum <makalu171@gmail.com>
2022-03-02 22:32:20 +0900

    🎨 code formatting

--- src/api/index.js
+++ src/api/index.js
@@ -1,45 +1,46 @@
 import { Notyf } from 'notyf'
 
 const URLS = {
-	BookStatus: '/api/bookExist',
-	BookList : 'https://dapi.kakao.com/v3/search/book',
+  BookStatus: '/api/bookExist',
+  BookList: 'https://dapi.kakao.com/v3/search/book',
 }
 
 const request = async (url, params, option) => {
-	const urlSearchParams = new URLSearchParams(params)
-	const response = await fetch(`${url}?${urlSearchParams}`, option)
-	try {
-		if (!response.ok) {
-			const notyf = new Notyf({
-				dismissible: true,
-			})
-			notyf.error('에러가 발생했습니다.')
-			console.error(response)
-		}
-		return response.json()
-	} catch (err) {
-		return err
-	}
+  const urlSearchParams = new URLSearchParams(params)
+  const response = await fetch(`${url}?${urlSearchParams}`, option)
+  try {
+    if (!response.ok) {
+      const notyf = new Notyf({
+        dismissible: true,
+      })
+      notyf.error('에러가 발생했습니다.')
+      console.error(response)
+    }
+    return response.json()
+  } catch (err) {
+    return err
+  }
 }
 
 const BookApi = {
-	async getBookStatus(isbn13) {
-		const authKey = import.meta.env.VITE_LIBRARY_API_KEY
-		const libCode = '121014'
-		return request(URLS.BookStatus, {
-			authKey,
-			libCode, 
-			isbn13,
-			format: 'json',
-		})
-	},
-	async getBookList(params) {
-		return request(URLS.BookList, params, {
-			headers: {
-				Authorization: import.meta.env.VITE_KAKAO_API_KEY,
-			}
-		})
-	}
+  async getBookStatus(isbn13) {
+    const authKey = import.meta.env.VITE_LIBRARY_API_KEY
+    const libCode = '121014'
+    console.log(`request`)
+    return request(URLS.BookStatus, {
+      authKey,
+      libCode,
+      isbn13,
+      format: 'json',
+    })
+  },
+  async getBookList(params) {
+    return request(URLS.BookList, params, {
+      headers: {
+        Authorization: import.meta.env.VITE_KAKAO_API_KEY,
+      },
+    })
+  },
 }
 
 export default BookApi
